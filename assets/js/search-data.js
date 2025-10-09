74d894bc6f012c399b551b275f7a0278da66228e
datngu <59732388+datngu@users.noreply.github.com>
2025-10-09 13:54:29 +0000

    Deploying to gh-pages from @ AI-genome/academic@3e71b642aa63cb5c6692349818da6ff357e012da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -521,13 +521,6 @@ ninja.data = [{
           window.open("https://github.com/datngu", "_blank");
         },
       },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/ndat", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
